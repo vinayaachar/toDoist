fbfe38c880c23e08f0c600752faf3c1af110a1c6
vinayaachar <vinayaachar@gmail.com>
2021-07-15 22:20:49 -0700

    first commit

---
+++ todoist_app/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello there!
+        </p>
+      </header>
+    </div>
+  );
+}
+
